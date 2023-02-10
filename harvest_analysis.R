5ef3fc86c077d073f5b7c3c0d100d58525ff2515
Elspeth Ready <elspethready@ecod128.eva.mpg.de>
2023-02-10 11:30:21 +0100

    fine tuning figure again

--- harvest_analysis.R
+++ harvest_analysis.R
@@ -279,7 +279,7 @@ library(rphylopic)
 cols <- (viridis(4))[c(2,3,4,1)]
 cols[3] <- "chocolate4"
 
-caribou <- image_data("e6e864fd-8e3d-435f-9db3-dc6869c589f1", size = "thumb")[[1]]
+caribou <- image_data("e6e864fd-8e3d-435f-9db3-dc6869c589f1", size = 128)[[1]]
 char <- image_data("68304786-f291-4115-9ccd-ead068ba6f19", size = "thumb")[[1]]
 goose <- image_data("7b8fb3d4-0cac-4552-8cd1-bd493b7de679", size="thumb")[[1]]
 
@@ -313,12 +313,12 @@ dev.off()
 
 # lines from samples drawn from posterior??
 png("Figure2.png", height=4, width=6, 
-    units="in", res=480, pointsize=8)
-par(mar=c(1,4,3,1), cex.main=0.9)
+    units="in", res=480, pointsize=10)
+par(mar=c(1,4,3,1), cex.main=0.99)
 layout(matrix(c(1,2,3,4), ncol=4, byrow=TRUE), 
   widths=c(1.75,1.75,3,1))
-nsamps <- 500
-alp <- 0.05
+nsamps <- 100
+alp <- 0.15
 pullasamp <- sample(1:6000, nsamps) 
 plot(density(samps$harvest_est[,7,1]/1000)$y, 
      density(samps$harvest_est[,7,1]/1000)$x, xlab="", type="n", xaxt="n",
@@ -326,12 +326,12 @@ plot(density(samps$harvest_est[,7,1]/1000)$y,
      ylim=c(5, 140))
 for (i in 1:4) {
     abline(h=samps$harvest_est[pullasamp,7,i]/1000, 
-           col=alpha(cols[i], alp), lwd=2)
+           col=alpha(cols[i], alp))
 }
 box()
 plot(density(samps$market_cost_est[,7,1]/1000000)$y, 
      density(samps$market_cost_est[,7,1]/1000000)$x, xlab="", type="n", xaxt="n",
-     main="(b) Replacement value", ylab="Million $", xlim=c(0,100), 
+     main="(b) Substitution value", ylab="Million $", xlim=c(0,100), 
      ylim=c(0.1, 3.4))
 for (i in 1:4) {
   abline(h=samps$market_cost_est[pullasamp,7,i]/1000000, 
@@ -341,7 +341,7 @@ box()
 plot(density(samps$carbon_cost_est[,7,1,1]/1000)$y, 
      density(samps$carbon_cost_est[,7,1,1]/1000)$x, xlab="", 
      type="n",  xaxt="n",
-     main=expression(bold(paste("(c) ", CO[2], " emitted by replacements"))), 
+     main=expression(bold(paste("(c) ", CO[2], " emitted by substitutes"))), 
      ylab="Tonnes", xlim=c(0.15,3.85), ylim=c(20, 1200))
 for (i in 1:4) {
     for (j in 1:4) {
@@ -355,26 +355,26 @@ for (i in 1:4) {
 abline(v=1:3, lwd=1)
 text(x=c(0.5,1.5,2.5,3.5), y=c(0,0,0,0),
     labels=c("Low Barge", "High Barge", "Low Food Mail", "High Food Mail"),
-    cex=0.9)
+    cex=0.7)
 box()
 par(mar=c(1,0,3,0))
 plot(1:1000/5, 1:1000, type="n", axes=FALSE, ylab="", xlab="")
-add_phylopic_base(goose, x=23, y = 50, ysize=64,
+add_phylopic_base(goose, x=23, y = 50, ysize=60,
                   alpha = 1, col = cols[1])
-text(75, 50, "Birds", adj=0)
-add_phylopic_base(char, x=23, y = 120, ysize=64,
+text(80, 50, "Birds", adj=0)
+add_phylopic_base(char, x=23, y = 120, ysize=60,
                   alpha = 1, col = cols[2])
-text(75, 120, "Fish", adj=0)
-add_phylopic_base(caribou, x=30, y = 200, ysize=76,
+text(80, 120, "Fish", adj=0)
+add_phylopic_base(caribou, x=30, y = 200, ysize=72,
                   alpha = 1, col = cols[3])
-text(75, 200, "Mammals", adj=0)
+text(80, 200, "Mammals", adj=0)
 add_phylopic_base(goose, x=45, y = 275, ysize=38,
                   alpha = 1, col = cols[4])
 add_phylopic_base(caribou, x=15, y = 300, ysize=42,
                   alpha = 1, col = cols[4])
 add_phylopic_base(char, x=10, y = 260, ysize=34,
                   alpha = 1, col = cols[4])
-text(75, 275, "Total", adj=0)
+text(80, 275, "Total", adj=0)
 dev.off()
 
 
