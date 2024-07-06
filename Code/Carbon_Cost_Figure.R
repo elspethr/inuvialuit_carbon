95ecb23224eb9094cde265f3c61098baf5a06f8a
Elspeth Ready <eready@ualberta.net>
2024-07-06 12:43:02 +0200

    fix figure panel labels

--- Code/Carbon_Cost_Figure.R
+++ Code/Carbon_Cost_Figure.R
@@ -10,8 +10,8 @@ caribou = get_phylopic("e6e864fd-8e3d-435f-9db3-dc6869c589f1")
 char = get_phylopic("68304786-f291-4115-9ccd-ead068ba6f19")
 goose = get_phylopic("7b8fb3d4-0cac-4552-8cd1-bd493b7de679")
 
-png("Figure2.png", height=4, width=6, 
-    units="in", res=480, pointsize=10)
+tiff("Figure2.tiff", height=4, width=6, 
+    units="in", res=400, pointsize=10)
 par(mar=c(1,4,3,1), cex.main=0.99)
 layout(matrix(c(1,2,3,4), ncol=4, byrow=TRUE), 
   widths=c(1.75,1.75,3,1))
@@ -20,7 +20,7 @@ alp = 0.15
 pullasamp = sample(1:6000, nsamps) 
 plot(density(weight[[4]]/1000)$y, 
      density(weight[[4]]/1000)$x, xlab="", type="n", xaxt="n",
-     main="(a) Edible weight", ylab="Tonnes", xlim=c(0,1), 
+     main="(A) Edible weight", ylab="Tonnes", xlim=c(0,1), 
      ylim=c(5, 130))
 for (i in 1:4) {
     abline(h=weight[[i]][pullasamp]/1000, 
@@ -29,7 +29,7 @@ for (i in 1:4) {
 box()
 plot(density(cost[[4]]/1000000)$y, 
      density(cost[[4]]/1000000)$x, xlab="", type="n", xaxt="n",
-     main="(b) Substitution value", ylab="Million $", xlim=c(0,100), 
+     main="(B) Substitution value", ylab="Million $", xlim=c(0,100), 
      ylim=c(0.1, 3.4))
 for (i in 1:4) {
   abline(h=cost[[i]][pullasamp]/1000000, 
@@ -39,7 +39,7 @@ box()
 plot(density(emissions[[4]][[4]]/1000)$y, 
      density(emissions[[4]][[4]]/1000)$x, xlab="", 
      type="n",  xaxt="n",
-     main=expression(bold(paste("(c) ", CO[2], " emitted by substitutes"))), 
+     main=expression(bold(paste("(C) ", CO[2], " emitted by substitutes"))), 
      ylab="Tonnes", xlim=c(0.15,3.85), ylim=c(20, 1200))
 for (i in 1:4) {
     for (j in 1:4) {
@@ -58,19 +58,19 @@ box()
 par(mar=c(1,0,3,0))
 plot(1:1000/5, 1:1000, type="n", axes=FALSE, ylab="", xlab="")
 add_phylopic_base(goose, x=23, y = 50, ysize=60,
-                  alpha = 1, col = cols[1])
+                  alpha = 1, fill = cols[1])
 text(80, 50, "Birds", adj=0)
 add_phylopic_base(char, x=23, y = 120, ysize=60,
-                  alpha = 1, col = cols[2])
+                  alpha = 1, fill = cols[2])
 text(80, 120, "Fish", adj=0)
 add_phylopic_base(caribou, x=30, y = 200, ysize=72,
-                  alpha = 1, col = cols[3])
+                  alpha = 1, fill = cols[3])
 text(80, 200, "Mammals", adj=0)
 add_phylopic_base(goose, x=45, y = 275, ysize=38,
-                  alpha = 1, col = cols[4])
+                  alpha = 1, fill = cols[4])
 add_phylopic_base(caribou, x=15, y = 300, ysize=42,
-                  alpha = 1, col = cols[4])
+                  alpha = 1, fill = cols[4])
 add_phylopic_base(char, x=10, y = 260, ysize=34,
-                  alpha = 1, col = cols[4])
+                  alpha = 1, fill = cols[4])
 text(80, 275, "Total", adj=0)
 dev.off()
